eecd0949fdb6b28503a1bf1554f231f3379988cc
ryankoch13 <ryan.koch13@gmail.com>
2025-10-23 14:37:05 -0400

    feat: add expo router
    Signed-off-by: ryankoch13 <ryan.koch13@gmail.com>

---
+++ src/app/about.tsx
@@ -0,0 +1,10 @@
+import {Link} from 'expo-router';
+import {View, Text} from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <View className='flex-1 items-center justify-center'>
+            <Text className='text-3xl'>About</Text>
+        </View>
+    )
+}
